64a5d702f3a233dde40b40c50dfb6fef8492c783
Sihan Tan <shhhhhdev@gmail.com>
2023-08-31 23:16:22 +0800

    consolidate all premake files into 1 master premake file

--- premake5.lua
+++ premake5.lua
@@ -22,9 +22,127 @@ IncludeDir["glm"]           = "Overlord/vendor/glm"
 IncludeDir["stb_image"]     = "Overlord/vendor/stb_image"
 
 group "Library"
-    include "Overlord/vendor/GLFW"
-    include "Overlord/vendor/Glad"
-    include "Overlord/vendor/imgui"
+    --include "Overlord/vendor/GLFW"
+    project "GLFW"
+        location "Overlord/vendor/GLFW"
+
+        kind "StaticLib"
+
+        language "C"
+
+        targetdir ("Overlord/vendor/GLFW/bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("Overlord/vendor/GLFW/bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "Overlord/vendor/GLFW/include/**.h",
+            "Overlord/vendor/GLFW/src/**.h",
+            "Overlord/vendor/GLFW/src/**.c"
+        }
+
+        filter "system:windows"
+            systemversion "latest"
+            staticruntime "on"
+
+            files
+            {
+                "Overlord/vendor/GLFW/src/**.c"
+            }
+
+            defines
+            {
+                "_GLFW_WIN32",
+                "_CRT_SECURE_NO_WARNINGS"
+            }
+
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
+
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "on"
+
+    --include "Overlord/vendor/Glad"
+    project "Glad"
+        location "Overlord/vendor/Glad"
+
+        kind "StaticLib"
+        staticruntime "on"
+
+        language "C"
+
+        targetdir ("Overlord/vendor/Glad/bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("Overlord/vendor/Glad/bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "Overlord/vendor/Glad/include/glad/glad.h",
+            "Overlord/vendor/Glad/include/KHR/khrplatform.h",
+            "Overlord/vendor/Glad/src/glad.c"
+        }
+
+        includedirs
+        {
+            "Overlord/vendor/Glad/include"
+        }
+
+        filter "system:windows"
+            systemversion "latest"
+
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
+
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "on"
+    --include "Overlord/vendor/imgui"
+    project "ImGui"
+        location "Overlord/vendor/imgui"
+
+        kind "StaticLib"
+        staticruntime "on"
+        
+        language "C++"
+        cppdialect "C++17"
+
+        targetdir ("Overlord/vendor/imgui/bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("Overlord/vendor/imgui/bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            -- "Overlord/vendor/imgui/imconfig.h",
+            -- "Overlord/vendor/imgui/imgui.h",
+            -- "Overlord/vendor/imgui/imgui.cpp",
+            -- "Overlord/vendor/imgui/imgui_draw.cpp",
+            -- "Overlord/vendor/imgui/imgui_internal.h",
+            -- "Overlord/vendor/imgui/imgui_widgets.cpp",
+            -- "Overlord/vendor/imgui/imgui_tables.cpp",
+            -- "Overlord/vendor/imgui/imstb_rectpack.h",
+            -- "Overlord/vendor/imgui/imstb_textedit.h",
+            -- "Overlord/vendor/imgui/imstb_truetype.h",
+            -- "Overlord/vendor/imgui/imgui_demo.cpp"
+            "Overlord/vendor/imgui/*.h",
+            "Overlord/vendor/imgui/*.cpp"
+        }
+
+        filter "system:windows"
+            systemversion "latest"
+
+        filter "system:linux"
+            pic "On"
+            systemversion "latest"
+            cppdialect "C++17"
+            staticruntime "On"
+
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
+
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "on"
 group ""
 
 project "Overlord"
